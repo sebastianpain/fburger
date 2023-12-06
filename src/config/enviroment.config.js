0f5542296a94a94b8c1aa43fde7701e9a03de91e
Sebastian Ramirez <sebastianramirezpain@gmail.com>
2023-12-06 12:42:49 -0300

    1

--- src/config/enviroment.config.js
+++ src/config/enviroment.config.js
@@ -2,14 +2,14 @@ import dotenv from "dotenv";
 import { Command } from "commander";
 
 const program = new Command();
-program.option("--mode <mode>", "production", "DEVELOPMENT");
+program.option("--mode <mode>", "Modo de Trabajo", "DEVELOPMENT");
 program.parse();
 
 dotenv.config({
   path:
     program.opts().mode === "DEVELOPMENT"
       ? "./.env.development"
-      : "./.env"
+      : "./.env.production",
 });
 
 export default {
